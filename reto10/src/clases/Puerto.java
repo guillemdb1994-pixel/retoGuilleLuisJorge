adc0f31ca7d36aba1e88d05a1591d2c0ffabdb06
Luis Parra <parra.chaconluis006@gmail.com>
2026-01-19 13:26:04 +0100

    actualizacion de puerto

--- reto10/src/clases/Puerto.java
+++ reto10/src/clases/Puerto.java
@@ -23,6 +23,7 @@ public class Puerto {
 		return "Puerto [lista=" + lista + "]";
 	}
 	
+	// ENTRA BARCO
 	public void entrarBarco(Barco barco) {
 		if (lista.size()>=6) {
 			System.out.println("Puerto lleno");
@@ -31,6 +32,60 @@ public class Puerto {
 		}
 	}
 	
+	// SALIR BARCO
+	
+	public boolean salirBarco(String nombre) {
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getNombre().equalsIgnoreCase(nombre)) {
+                lista.remove(i);
+                System.out.println("Barco " + nombre + " ha salido del puerto.");
+                return true;
+            }
+        }
+        System.out.println("El barco no está en el puerto.");
+        return false;
+    }
+	
+	// CALCULAR PRECIO VIAJE
+	
+	
+    public double calcularPrecioViaje(Barco b, int horas, double precioCombustible) {
+    	
+        if (b.calcularAutonomia() < horas) {
+            return -1;
+        }
+        
+        return horas * b.getMotor().getConsumo() * precioCombustible;
+    }
+    
+    
+    
+    
+    
 	
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
